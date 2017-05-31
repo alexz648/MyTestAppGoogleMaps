194b50a2465950891ec153ab517eae5ec120c2fa
alexz648 <alexz648@gmail.com>
2017-05-31 09:26:06 -0700

    updated MapsActivity and activity_maps
    currently implementing location manager and tracker, as well as a search
    function

--- app/src/main/java/com/example/zhanga7141/mytestapp/MapsActivity.java
+++ app/src/main/java/com/example/zhanga7141/mytestapp/MapsActivity.java
@@ -2,11 +2,18 @@ package com.example.zhanga7141.mytestapp;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.icu.text.LocaleDisplayNames;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -15,9 +22,19 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
+import java.util.List;
+
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private LocationManager locationManager;
+    private boolean isGpsEnabled = false;
+    private boolean isNetworkEnabled = false;
+    private boolean canGetLocation = false;
+    private static final long MIN_TIME_BETWEEN_UPDATES = 1000*15;
+    private static final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 5.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,4 +82,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         }
     }
+
+    public void getLocation() {
+
+        try {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+            //get gps status
+            isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+            if (isGpsEnabled == true) {
+                Log.d("MyMaps", "getLocation: GPS is enabled");
+            }
+
+            //get network status
+            isNetworkEnabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+            if (isNetworkEnabled == true) {
+                Log.d("MyMaps", "getLocation: Network is enabled");
+            }
+
+            if (!isGpsEnabled && !isNetworkEnabled) {
+                Log.d("MyMaps", "getLocation: No Provider is enabled");
+
+            } else {
+                canGetLocation = true;
+                if(isGpsEnabled == true) {
+                    Log.d("MyMaps", "getLocation: GPS enabled & requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+                    Log.d("MyMaps", "getLocation: Network GPS update request is happening");
+                    Toast.makeText(this, "Currently Using GPS", Toast.LENGTH_SHORT).show();
+                }
+                if(isNetworkEnabled== true) {
+                    Log.d("MyMaps", "getLocation: Network enabled & requesting location updates");
+                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, MIN_TIME_BETWEEN_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListenerNetwork);
+                    Log.d("MyMaps", "getLocation: Network GPS update request is happening");
+                    Toast.makeText(this, "Currently Using GPS", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+
+
+        } catch (Exception e) {
+            Log.d("MyMaps", "Caught an exception in getLocation");
+            e.printStackTrace();
+        }
+
+    }
+
+    public void trackMe() {
+        getLocation();
+
+    }
+    public void searchPlaces(View view) {
+        EditText locationSearch = (EditText) findViewById(R.id.searchField);
+        String location = locationSearch.getText().toString();
+        List<Address> addressList = null;
+
+        if (location != null || !location.equals("")) {
+            Geocoder geocoder = new Geocoder(this);
+            try {
+                addressList = geocoder.getFromLocationName(location, 1);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            Address address = addressList.get(0);
+            LatLng latLng = new LatLng(address.getLatitude(), address.getLongitude());
+            mMap.addMarker(new MarkerOptions().position(latLng).title("Search Results"));
+            mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        }
+    }
+
 }
